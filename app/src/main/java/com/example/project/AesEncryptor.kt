33119912e58f49f24a8f94c5730abcbef105b7d1
vaishak109 <vaishak.ap@gadgeon.com>
2021-06-22 12:02:14 +0530

    Add AesEncryptor.kt

--- app/src/main/java/com/example/project/AesEncryptor.kt
+++ app/src/main/java/com/example/project/AesEncryptor.kt
@@ -1,4 +1,38 @@
 package com.example.project
 
+import java.security.SecureRandom
+import javax.crypto.Cipher
+import javax.crypto.KeyGenerator
+import javax.crypto.SecretKey
+
 class AesEncryptor {
+    /**
+     *generates a secret key required for aes encryption using KeyGenerator class
+     * @return generated SecretKey
+     */
+    fun generateKey(): SecretKey {
+        val keygen = KeyGenerator.getInstance(AES)
+        val random = SecureRandom()
+        keygen.init(KEY_SIZE, random)
+        return keygen.generateKey()
+    }
+
+    /**
+     *encrypt the ByteArray using Cipher class with "AES/ECB/PKCS5Padding" transformation
+     * @param data the input byteArray to encrypt
+     * @param key the secret key required for encryption
+     * @return encrypted ByteArray
+     */
+    fun encrypt(data: ByteArray, key: SecretKey): ByteArray {
+        val cipher = Cipher.getInstance(TRANSFORMATION)
+        cipher.init(Cipher.ENCRYPT_MODE, key)
+        return cipher.doFinal(data)
+    }
+
+    companion object {
+        const val AES = "AES"
+        const val KEY_SIZE = 256
+        const val IV_SIZE = 16
+        const val TRANSFORMATION = "AES/ECB/PKCS5Padding"
+    }
 }
