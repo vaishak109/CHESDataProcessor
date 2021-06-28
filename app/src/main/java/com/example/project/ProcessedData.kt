2b05980cbcb4ad8d206d8f5e62dc5e77a5528bc9
vaishak109 <85877703+vaishak109@users.noreply.github.com>
2021-06-28 15:54:24 +0530

    Update ProcessedData.kt
    Updated parameter name

--- app/src/main/java/com/example/project/ProcessedData.kt
+++ app/src/main/java/com/example/project/ProcessedData.kt
@@ -1,6 +1,6 @@
 package com.example.project
 
-class ProcessedData (aesKey: ByteArray?, outputData: ByteArray, hashKey: ByteArray?) {
+class ProcessedData (encryptedAesKey: ByteArray?, outputData: ByteArray, hashKey: ByteArray?) {
     val aesKey: ByteArray? = aesKey
     val outputData: ByteArray = outputData
     val hashKey: ByteArray? = hashKey
