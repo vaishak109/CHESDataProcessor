43ac826f0aac6282b49db06be3dfa9b8b4fc3058
vaishak109 <vaishak.ap@gadgeon.com>
2021-06-30 12:05:58 +0530

    Add few changes to RsaEncryptor.kt

--- app/src/main/java/com/example/project/RsaEncryptor.kt
+++ app/src/main/java/com/example/project/RsaEncryptor.kt
@@ -1,17 +1,28 @@
 package com.example.project
 
+import android.os.Build
+import android.util.Base64
 import java.security.PublicKey
 import javax.crypto.Cipher
 import java.security.spec.X509EncodedKeySpec
 import java.security.KeyFactory
+import java.security.spec.MGF1ParameterSpec
+import javax.crypto.spec.OAEPParameterSpec
+import javax.crypto.spec.PSource
 
-class RsaEncryptor (publicKey: String){
 
-    private val publicKey: PublicKey
-    init {
-        val publicKeySpec = X509EncodedKeySpec(publicKey.toByteArray())
+class RsaEncryptor (){
+
+    /**
+     *returns the public key needed for RSA encryption from the given Base64 encoded Public Key string
+     * @param publicKeyString a Base64 encoded RSA Public Key string
+     * @return RSA Public Key
+     */
+    fun getPublicKeyFromBase64EncodedString(publicKeyString: String): PublicKey {
+        val keyBytes = Base64.decode(publicKeyString, Base64.DEFAULT)
+        val publicKeySpec = X509EncodedKeySpec(keyBytes)
         val keyFactory = KeyFactory.getInstance(RSA);
-        this.publicKey = keyFactory.generatePublic(publicKeySpec);
+        return keyFactory.generatePublic(publicKeySpec);
     }
 
     /**
@@ -19,13 +30,18 @@ class RsaEncryptor (publicKey: String){
      * @param data the input byteArray to encrypt
      * @return encrypted ByteArray
      */
-    fun encrypt(data: ByteArray): ByteArray {
-        val cipher = Cipher.getInstance(RSA)
-        cipher.init(Cipher.ENCRYPT_MODE, this.publicKey)
+    fun encrypt(data: ByteArray, publicKey: PublicKey): ByteArray {
+        val cipher = Cipher.getInstance(node_rsa_init)
+        cipher.init(Cipher.ENCRYPT_MODE, publicKey)
+        cipher.init(
+            Cipher.ENCRYPT_MODE, publicKey,
+            OAEPParameterSpec("SHA-1", "MGF1", MGF1ParameterSpec.SHA1, PSource.PSpecified.DEFAULT)
+        )
         return cipher.doFinal(data)
     }
 
     companion object {
         const val RSA = "RSA"
+        const val node_rsa_init = "RSA/ECB/OAEPWithSHA1AndMGF1Padding"
     }
 }
